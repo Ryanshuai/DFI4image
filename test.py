7a84a4d66bff2e31545462fb001a9191a4b163dc
Ryan <1018718155@qq.com>
2018-01-16 11:33:50 +0800

    1.16 早上 修改成果

--- test.py
+++ test.py
@@ -1,11 +1,7 @@
 
 import numpy
-import deepmodels_torch
+import deepmodel
 import json
-import time
-import argparse
-import os.path
-import subprocess
 import utils
 
 with open('datasets/lfw/lfw_binary_attributes.json') as f: lfw=json.load(f)
@@ -41,44 +37,25 @@ def make_manifolds(a,s=[],t=[],N=10,X=None,visualize=False):
     return [lfw_filelist[x] for x in X],[[lfw_filelist[x] for x in y] for y in P],[[lfw_filelist[x] for x in y] for y in Q]
 
 if __name__=='__main__':
-    # configure by command-line arguments
-    parser=argparse.ArgumentParser(description='Generate LFW face transformations.',formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument('--backend',type=str,default='caffe+scipy',choices=['torch','caffe+scipy'],help='reconstruction implementation')
-    parser.add_argument('--device_id',type=int,default=0,help='zero-indexed CUDA device')
-    parser.add_argument('--K',type=int,default=100,help='number of nearest neighbors')
-    parser.add_argument('--scaling',type=str,default='beta',choices=['none','beta'],help='type of step scaling')
-    parser.add_argument('--iter',type=int,default=500,help='number of reconstruction iterations')
-    parser.add_argument('--postprocess',type=str,default='color',help='comma-separated list of postprocessing operations')
-    parser.add_argument('--delta',type=str,default='0.4',help='comma-separated list of interpolation steps')
-    config=parser.parse_args()
-    postprocess=set(config.postprocess.split(','))
-    print(json.dumps(config.__dict__))
+
+    K = 100
+    max_iter = 500
+    delta_list = [0.4]
+    color_postprocess = True
 
     # load CUDA model
     minimum_resolution=200
-    model=deepmodels_torch.vgg19g_torch(device_id=config.device_id)
-
-    # download AEGAN cropped+aligned LFW (if needed)
-    if not os.path.exists('images/lfw_aegan'):
-        url='https://www.dropbox.com/s/isz4ske2kheuwgr/lfw_aegan.tar.gz?dl=1'
-        subprocess.check_call(['wget',url,'-O','lfw_aegan.tar.gz'])
-        subprocess.check_call(['tar','xzf','lfw_aegan.tar.gz'])
-        subprocess.check_call(['rm','lfw_aegan.tar.gz'])
+    model=deepmodel.vgg19g_torch(device_id=0)
 
     # read test data
     data=numpy.load('tests/dmt2-lfw-multiple-attribute-test.npz')
     pairs=list(data['pairs'][[0,1,2,4,5,6]]) # skip flushed face, not interesting
     X=data['X']
 
-    # comment out the line below to generate all the test images
-    X=X[:1]
 
     # Set the free parameters
     # Note: for LFW, 0.4*8.82 is approximately equivalent to beta=0.4
-    K=config.K
-    delta_params=[float(x.strip()) for x in config.delta.split(',')]
 
-    t0=time.time()
     result=[]
     original=[]
     # for each test image
@@ -102,31 +79,26 @@ if __name__=='__main__':
             Q=Q[0]
             xP=[x.replace('lfw','lfw_aegan') for x in P]
             xQ=[x.replace('lfw','lfw_aegan') for x in Q]
-            PF=model.get_Deep_Feature(utils.image_feed(xP[:K],image_dims))
-            QF=model.get_Deep_Feature(utils.image_feed(xQ[:K],image_dims))
-            if config.scaling=='beta':
+            PF=model.get_Deep_Feature(utils.im_generator(xP[:K],image_dims))
+            QF=model.get_Deep_Feature(utils.im_generator(xQ[:K],image_dims))
+            if True:
                 WF=(QF-PF)/((QF-PF)**2).mean()
-            elif config.scaling=='none':
+            else:
                 WF=(QF-PF)
-            max_iter=config.iter
             init=o
             # for each interpolation step
-            for delta in delta_params:
+            for delta in delta_list:
                 print(xX,b,delta)
-                t2=time.time()
                 Y=model.Deep_Feature_inverse(XF+WF*delta,max_iter=max_iter,initial_image=init)
-                t3=time.time()
-                print('{} minutes to reconstruct'.format((t3-t2)/60.0))
                 result[-1].append(Y)
                 max_iter=config.iter//2
                 init=Y
+
     result=numpy.asarray(result)
     original=numpy.asarray(original)
-    if 'color' in postprocess:
+    if color_postprocess:
         result=utils.color_match(numpy.expand_dims(original,1),result)
     m=utils.montage(numpy.concatenate([numpy.expand_dims(original,1),result],axis=1))
     utils.im_write('results/demo1.png',m)
     print('Output is results/demo1.png')
-    t1=time.time()
-    print('{} minutes ({} minutes per image).'.format((t1-t0)/60.0,(t1-t0)/60.0/result.shape[0]/result.shape[1]))
 
--- utils.py
+++ utils.py
@@ -1,37 +1,8 @@
 
 import numpy
-import os.path
 import PIL.Image
 import PIL.ImageDraw
 import PIL.ImageFont
-
-
-
-def im_read(ipath,dtype=numpy.float32):
-    '''
-    Returns a H x W x 3 RGB image in the range of [0,1].
-    '''
-    img=PIL.Image.open(ipath)
-    if img.mode!='RGB':
-        img=img.convert('RGB')
-    return numpy.asarray(img,dtype=dtype)/255.0
-
-def im_write(opath,I,**kwargs):
-    '''
-    Given a H x W x 3 RGB image it is clipped to the range [0,1] and
-    written to an 8-bit image file.
-    '''
-    img=PIL.Image.fromarray((I*255).clip(0,255).astype(numpy.uint8))
-    ext=os.path.splitext(opath)[1]
-    if ext=='.jpg':
-        quality=kwargs['quality'] if 'quality' in kwargs else 95
-        img.save(opath,quality=quality,optimize=True)
-    elif ext=='.png':
-        img.save(opath)
-    else:
-        # I do not want to save unknown extensions because there is no
-        # expectation that the default save options are reasonable.
-        raise ValueError('Unknown image extension ({})'.format(ext))
 
 def im_resize(I,shape):
     image=PIL.Image.fromarray((I*255).clip(0,255).astype(numpy.uint8))
@@ -40,6 +11,24 @@ def im_resize(I,shape):
     else:
         image=image.resize((shape[1],shape[0]),PIL.Image.LANCZOS)
     return numpy.asarray(image)/255.0
+
+def im_read(image_path,dtype=numpy.float32):
+    img=PIL.Image.open(image_path)
+    if img.mode!='RGB':
+        img=img.convert('RGB')
+    return numpy.asarray(img,dtype=dtype)/255.0
+
+def im_write(opath,image):
+    img=PIL.Image.fromarray((image*255).clip(0,255).astype(numpy.uint8))
+    img.save(opath)
+
+def im_generator(paths,image_size):
+    for x in paths:
+        I=im_read(x)
+        if I.shape[:2]!=image_size:
+            yield im_resize(I,tuple(image_size))
+        else:
+            yield I
 
 def montage(M,sep=0,canvas_value=0):
     # row X col X H X W X C
@@ -69,17 +58,6 @@ def concatenate(X,axis,canvas_value=0,gravity=(-1)):
         else:
             Y.append(numpy.pad(x,[(0,newshape[i]-x.shape[i]) for i in range(x.ndim)],'constant',constant_values=canvas_value))
     return numpy.concatenate(Y,axis=axis)
-
-def image_feed(S,image_dims):
-    '''
-    Given a list of file paths and a 2-tuple of (H, W), yields H x W x 3 images.
-    '''
-    for x in S:
-        I=im_read(x)
-        if I.shape[:2]!=image_dims:
-            yield im_resize(I,tuple(image_dims))
-        else:
-            yield I
 
 def color_match(A,B):
     '''
